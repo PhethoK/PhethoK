2d2ff597059813d939936aaa6ab06808755aff07
PhethoK <100271868+PhethoK@users.noreply.github.com>
2024-01-15 10:52:49 +0200

    script.js

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ $(window).on("scroll", function () {
     }
   });
 });
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 600px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
